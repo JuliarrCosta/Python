abf2aa83f8da25dbe029f449470ec9c2a18ee58b
Julia Costa <Juliaraquelrc@outlook.com>
2024-02-04 10:38:30 -0300

    Adicionando database

--- AgendaTelefonica/AgendaTelefonica.py
+++ AgendaTelefonica/AgendaTelefonica.py
@@ -1,8 +1,14 @@
 ##importando os elementos da classe contato
 from Contato import *
 class AgendaTelefonica:
+    ##instanciei uma lista e carregando o database
+    '''' Por ser um database básico em csv não houve necessidade de
+         criar uma função mais complexa para conectar o banco com servidor,
+         por isso decidi inicializar o database no 'construtor' '''
+    
     def __init__(self):
         self.agendaTelefonica = []
+        self.carregar()
     ##Retorna se o contatato é true ou false na lista
     def verificarContato(self, nome):
         for contato in self.agendaTelefonica:
@@ -17,13 +23,15 @@ class AgendaTelefonica:
         else:
             novo_contato= Contato(nome, numero)
             self.agendaTelefonica.append(novo_contato)
+            self.salvar()
+
     ##remove o contato
     def removerContato(self, nome):
         if self.verificarContato(nome):
              for contato in self.agendaTelefonica:
                 if contato.nome == nome:
                     self.agendaTelefonica.remove(contato)
-                
+                    self.salvar()           
         else:
             print(f'Contato não encotrado!')
     ##caso o contato tenha o nome especificado acima o novo numero será adicionado no antigo
@@ -31,6 +39,7 @@ class AgendaTelefonica:
         if self.verificarContato(nome):
             for contato in self.agendaTelefonica:
                 contato.numero = novoNumero
+                self.salvar()
         else:
             print(f'Contato não encontrado')
     ##percorre a lista e aciona o método imprimir_contato do objeto
@@ -45,9 +54,9 @@ class AgendaTelefonica:
             else:
                 print(f'Contato não encontrado')
     ##cria um arquivo csv se não existir e escreve dentro dele a agenda de contatos
-    def exportarContatos(self):
+    def exportarContatos(self, nomeArquivo):
         try: 
-            with open('Agenda.csv', 'w') as arquivo:
+            with open(nomeArquivo, 'w') as arquivo:
                 for contato in self.agendaTelefonica:
                     arquivo.write(contato.nome + ',')
                     arquivo.write(contato.numero + '\n')
@@ -64,13 +73,20 @@ class AgendaTelefonica:
                     nomeImportado = dado[0]
                     numeroImportado = dado[1]
                     self.adicionarContato(nomeImportado, numeroImportado)
+                    self.salvar()
         except FileNotFoundError as error:
             print('Arquivo não encontrado')
+    
+    def salvar(self):
+        self.exportarContatos('database.csv')
 
-
+    def carregar(self):
+        self.importarContatos('database.csv')
 
 ##TESTANDO O CÓDIGO
 agenda = AgendaTelefonica()
+
+''' 
 agenda.adicionarContato('Julia', '12232312')
 agenda.editarContato('Julia', '1234444')
 agenda.adicionarContato('Jose', '1232312')
@@ -82,6 +98,9 @@ print('--------------')
 agenda.adicionarContato('Julia', '12232312')
 agenda.imprimirContatos()
 print('--------------')
-agenda.exportarContatos()
 agenda.importarContatos('Agenda.txt')
+agenda.imprimirContatos() 
+
+'''
+
 agenda.imprimirContatos()
